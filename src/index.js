91506c7a6f937b7bbbaa80e6a489085e39a2c5a9
Kuba <jakub.jaszcz538@gmail.com>
2022-05-28 13:11:16 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import App from 'App';
+const container = document.getElementById('root');
+
+// Create a root.
+const root = ReactDOM.createRoot(container);
+
+// Initial render
+root.render(<App />);
+
+
