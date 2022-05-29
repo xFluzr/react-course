e2a9f27cb85e1a05c25724dd1dd5fb7975dc3039
Kuba <jakub.jaszcz538@gmail.com>
2022-05-29 20:25:01 +0200

    Second exercise

--- src/components/Expenses/ExpenseHolder.js
+++ src/components/Expenses/ExpenseHolder.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './ExpenseHolder.css';
+import ExpenseFilter from './ExpenseFilter';
 const ExpenseHolder = ({expenses}) => {
+  const [currentYear,setCurrentYear]=useState();
+
+  const selectYear=(e)=>{
+    setCurrentYear(e.target.value);
+    console.log(currentYear)
+  }
 
   return (
-    <Card className='expenses'>
+       <Card className='expenses'>
+           <ExpenseFilter onSelectedYear={selectYear}/>
         <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        price={expenses[0].price}
-      />
+          date={expenses[0].date}
+          title={expenses[0].title}
+          price={expenses[0].price}
+        />
 
       <ExpenseItem
         date={expenses[1].date}
         title={expenses[1].title}
-        price={expenses[1].price} />
+        price={expenses[1].price} 
+      />
 
       <ExpenseItem
         date={expenses[2].date}
         title={expenses[2].title}
-        price={expenses[2].price} />
+        price={expenses[2].price} 
+      />
     </Card>
+  
+   
   )
 }
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,28 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import "./ExpenseItem.css"
 import Card from "../UI/Card";
 
 const ExpenseItem = ({ title, price, date }) => {
 
-    const [propsTitle,setPropsTitle] = useState(title);
-
-    const clickHandler = ()=>{
+    const [propsTitle, setPropsTitle] = useState(title);
+    const [selectedYear,setSelectedYear] =useState("");
+    const clickHandler = () => {
         setPropsTitle("Updated");
     }
+
+   
 
 
     return (
         <Card className='expense-item'>
-             <div>
-                <ExpenseDate date={date}/>
+            <div>
+                <ExpenseDate date={date} />
             </div>
             <div className='expense-item__description'>
                 <h2>{propsTitle}</h2>
             </div>
-           
+
             <div className='expense-item__price'>
                 ${price}
             </div>
